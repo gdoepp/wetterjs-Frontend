924a94c0becb94e4843cc5369b246aff43b757f5
gd <gd>
2018-03-30 22:47:10 +0200

    cleanup, REST, e2e

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      environment: 'dev'
+      environment: 'local'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
