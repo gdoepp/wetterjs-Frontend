201ef540b80f82c62fcc1b7ef25cb8f0c7c9fa4f
gd <gd>
2020-07-31 17:12:12 +0200

    update dependencies

--- src/app/datatransfer.service.ts
+++ src/app/datatransfer.service.ts
@@ -2,17 +2,21 @@
 
 // this service informs the main component about changes triggered by some child below router-outlet
 
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
+
+class Data {
+  operation: string; time?: string; per?: string; value: string; stat?: string; link?: string;
+}
 
 @Injectable()
 export class DataTransferService {
 
-  private childData = new BehaviorSubject<any>([]);
+  private childData = new BehaviorSubject<Data>(new Data);
 
   fromChild = this.childData.asObservable();
 
-  sendToParent(data) {
+  sendToParent(data: Data) {
     this.childData.next(data);
   }
 }
